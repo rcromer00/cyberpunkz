77a820144c80eb7a1f029e8fcb78652d782d9dfb
rcromer00 <cromerro@msu.edu>
2022-10-31 22:46:04 -0400

    made some changes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const navBtn = document.getElementById("nav-btn");
 const navBar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
 
+
 // show nav
 navBtn.addEventListener("click", () => {
 	navBar.classList.add("showNav");
@@ -13,3 +14,15 @@ navClose.addEventListener("click", () => {
 	navBar.classList.remove("showNav");
 	navBtn.style.display = "block";
 });
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+   navBar.style.top = "0";
+  } else {
+    navBar.style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
